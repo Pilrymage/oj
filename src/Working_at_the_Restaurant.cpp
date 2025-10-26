81e0728887c9dbe812fc9636e02f6de9b5064137
pilrymage <for156th@gmail.com>
2025-10-26 20:01:41 +0800

    Working_at_the_Restaurant

---
+++ src/Working_at_the_Restaurant.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<string> vs;
+
+const int INF = 1e9;
+const int LLINF = 4e18;
+const double EPS = 1e-9;
+
+void __print(int x) { std::cerr << x; }
+void __print(long long x) { std::cerr << x; }
+void __print(double x) { std::cerr << x; }
+void __print(char x) { std::cerr << '\'' << x << '\''; }
+void __print(const char *x) { std::cerr << '\"' << x << '\"'; }
+void __print(const std::string &x) { std::cerr << '\"' << x << '\"'; }
+void __print(bool x) { std::cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const std::pair<T, V> &x)
+{
+    std::cerr << '{';
+    __print(x.first);
+    std::cerr << ',';
+    __print(x.second);
+    std::cerr << '}';
+}
+
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    std::cerr << '{';
+    for (auto &i : x)
+        std::cerr << (f++ ? "," : ""), __print(i);
+    std::cerr << "}";
+}
+
+void _print() { std::cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        std::cerr << ", ";
+    _print(v...);
+}
+
+#ifdef LOCAL
+#define debug(x...)                    \
+    std::cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+// string s = "hello";
+// int a = 10, b = 20;
+// vi v = {1,2,3,4,5};
+// debug(s, a, b, v);
+// for (int i = 0; i < 3; i++) {
+//     int result = i * 10;
+//     debug(i, result);
+// }
+// assert(index >= 0 && index < v.size());
+#define debug(x...)
+#endif
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    int f = 0;
+    while (cin >> n >> ws, n)
+    {
+        string line;
+        if (f++)
+            cout << endl;
+        int a = 0, b = 0;
+        for (int i = 0; i < n; i++)
+        {
+            getline(cin, line);
+            stringstream ss(line);
+            string op;
+            int m;
+            ss >> op >> m;
+
+            if (op == "DROP")
+            {
+                cout << "DROP 2 " << m << endl;
+                b += m;
+            }
+            else if (op == "TAKE")
+            {
+
+                if (a < m)
+                {
+                    if(a > 0) cout << "TAKE 1 " << a << endl;
+                    cout << "MOVE 2->1 " << b << endl;
+                    cout << "TAKE 1 " << m - a << endl;
+                    a += b-m;
+                    b = 0;
+                } else {
+                    cout << "TAKE 1 " << m << endl;
+                    a -= m;
+                    
+                }
+            }
+        }
+    }
+    return 0;
+}
