ac4bc1565e7e322777b31798d9077bef5565c553
acer <1>
2025-10-27 12:10:19 +0800

    Nesting_a_Bunch_of_Brackets

---
+++ src/Nesting_a_Bunch_of_Brackets.cpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<string> vs;
+
+const int INF = 1e9;
+const int LLINF = 4e18;
+const double EPS = 1e-9;
+
+void __print(int x) { std::cerr << x; }
+void __print(long long x) { std::cerr << x; }
+void __print(double x) { std::cerr << x; }
+void __print(char x) { std::cerr << '\'' << x << '\''; }
+void __print(const char *x) { std::cerr << '\"' << x << '\"'; }
+void __print(const std::string &x) { std::cerr << '\"' << x << '\"'; }
+void __print(bool x) { std::cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const std::pair<T, V> &x)
+{
+    std::cerr << '{';
+    __print(x.first);
+    std::cerr << ',';
+    __print(x.second);
+    std::cerr << '}';
+}
+
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    std::cerr << '{';
+    for (auto &i : x)
+        std::cerr << (f++ ? "," : ""), __print(i);
+    std::cerr << "}";
+}
+
+void _print() { std::cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        std::cerr << ", ";
+    _print(v...);
+}
+
+#ifdef LOCAL
+#define debug(x...)                    \
+    std::cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+// string s = "hello";
+// int a = 10, b = 20;
+// vi v = {1,2,3,4,5};
+// debug(s, a, b, v);
+// for (int i = 0; i < 3; i++) {
+//     int result = i * 10;
+//     debug(i, result);
+// }
+// assert(index >= 0 && index < v.size());
+#define debug(x...)
+#endif
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string line;
+    while (getline(cin, line))
+    {
+        debug(line);
+        stack<char> bracket;
+        auto it = line.begin();
+        int len = 1;
+
+        while (it != line.end())
+        {
+            char c = *it;
+            bool isOk = true;
+            debug(*it);
+            if (it != line.end() - 1 && c == '(' && *(it + 1) == '*')
+            {
+                bracket.push('/');
+                it += 2;
+            }
+            else if (it != line.end() - 1 && c == '*' && *(it + 1) == ')')
+            {
+                if (!bracket.empty() && bracket.top() == '/')
+                {
+                    bracket.pop();
+                    it += 2;
+                }
+                else
+                {
+                    cout << "NO " << len << endl;
+                    isOk = false;
+                }
+            }
+            else
+            {
+                map<char, char> bra = {
+                    {')', '('},
+                    {']', '['},
+                    {'}', '{'},
+                    {'>', '<'}};
+                switch (c)
+                {
+                case '(':
+                case '[':
+                case '{':
+                case '<':
+                    bracket.push(c);
+                    it++;
+                    break;
+                case ')':
+                case ']':
+                case '}':
+                case '>':
+                    if (!bracket.empty() && bracket.top() == bra[c])
+                    {
+                        bracket.pop();
+                        it++;
+                    }
+                    else
+                    {
+                        cout << "NO " << len << endl;
+                        isOk = false;
+                    }
+                    break;
+                default:
+                    it++;
+                }
+            }
+            
+            if (!isOk)
+                break;
+            else if (it == line.end()) {
+                if(!bracket.empty())
+                    cout << "NO " << ++len << endl;
+                else cout << "YES" << endl;
+            }
+             
+            len++;
+        }
+    }
+
+    return 0;
+}
