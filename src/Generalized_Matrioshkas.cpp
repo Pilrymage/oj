84897be180a5fe8179e2fd8ae4372b846eb78cbc
pilrymage <for156th@gmail.com>
2025-11-10 16:10:00 +0800

    Generalized_Matrioshkas

---
+++ src/Generalized_Matrioshkas.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<string> vs;
+
+const int INF = 1e9;
+const int LLINF = 4e18;
+const double EPS = 1e-9;
+
+void __print(int x) { std::cerr << x; }
+void __print(long long x) { std::cerr << x; }
+void __print(double x) { std::cerr << x; }
+void __print(char x) { std::cerr << '\'' << x << '\''; }
+void __print(const char *x) { std::cerr << '\"' << x << '\"'; }
+void __print(const std::string &x) { std::cerr << '\"' << x << '\"'; }
+void __print(bool x) { std::cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const std::pair<T, V> &x)
+{
+    std::cerr << '{';
+    __print(x.first);
+    std::cerr << ',';
+    __print(x.second);
+    std::cerr << '}';
+}
+
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    std::cerr << '{';
+    for (auto &i : x)
+        std::cerr << (f++ ? "," : ""), __print(i);
+    std::cerr << "}";
+}
+
+void _print() { std::cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        std::cerr << ", ";
+    _print(v...);
+}
+
+#ifdef LOCAL
+#define debug(x...)                    \
+    std::cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+// string s = "hello";
+// int a = 10, b = 20;
+// vi v = {1,2,3,4,5};
+// debug(s, a, b, v);
+// for (int i = 0; i < 3; i++) {
+//     int result = i * 10;
+//     debug(i, result);
+// }
+// assert(index >= 0 && index < v.size());
+#define debug(x...)
+#endif
+
+struct pack
+{
+    int size = 0;
+    int contain = 0;
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+    string line;
+    while (getline(cin, line))
+    {
+        // debug(line);
+        stack<pack> matryoshka;
+        stringstream ss(line);
+        int temp;
+        bool isPaired = true;
+        while (ss >> temp)
+        {
+            // debug(temp);
+            if (!matryoshka.empty() && matryoshka.top().size + temp == 0)
+            {
+                // debug("1");
+                matryoshka.pop();
+                // debug(matryoshka.top().size, matryoshka.top().contain);
+                if (!matryoshka.empty())
+                    matryoshka.top().contain += abs(temp);
+                if (!matryoshka.empty() && abs(matryoshka.top().contain) >= abs(matryoshka.top().size))
+                // 装不下套娃
+                {
+                    isPaired = false;
+                    break;
+                }
+                continue;
+            // }
+            // else if (!matryoshka.empty() && matryoshka.top().size * temp < 0)
+            // // 本该互相盖上的套娃对不上
+            // {
+            //     // debug("2");
+            //     isPaired = false;
+            //     break;
+            }else if (temp > 0) {
+                isPaired = false;
+                break;
+            }
+            else
+            {
+                matryoshka.push({temp, 0});
+            }
+        }
+        // debug(matryoshka.empty());
+        if (!isPaired || !matryoshka.empty())
+        {
+            cout << ":-( Try again." << endl;
+        }
+        else
+        {
+            cout << ":-) Matrioshka!" << endl;
+        }
+    }
+
+    return 0;
+}
